b0444e7960e9d45b0584a2591871d44ef3514b5e
simonescoca <simone.scoca@icloud.com>
2023-06-19 19:21:19 +0200

    added title & logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,12 @@
 <template>
-
+    <header class="container d-flex justify-content-start align-items-center">
+        <div class="my_img-container me-5">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Yu Gi Oh logo">
+        </div>
+        <h1 class="my_title">
+            Yu-Gi-Oh API
+        </h1>
+    </header>
 </template>
 
 <script>
@@ -27,4 +34,21 @@
 <style lang="scss" scoped>
     @use "../styles/variables.scss";
     @use "../styles/mixins.scss";
+
+    .my_img-container {
+        width: 100px;
+        height: 100px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+            object-position: center;
+        }
+    }
+
+    .my_title {
+        color: red;
+    }
+
 </style>
