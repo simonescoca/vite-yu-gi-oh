1b790059fdce78a3e6d8d4007df8d7074a7f4ad2
simonescoca <simone.scoca@icloud.com>
2023-06-19 17:55:47 +0200

    added bootstrap card layout

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,12 +1,27 @@
 <template>
-
+    <div class="card">
+        <img :src="ObjArrayYuGiOh[index].card_images[0].image_url"
+        class="card-img-top"
+        :alt="'image of ' + ObjArrayYuGiOh[index].name + ' card'">
+        <div class="card-body">
+            <h5 class="card-title">
+                {{ ObjArrayYuGiOh[index].name }}
+            </h5>
+            <p class="card-text">
+                {{ ObjArrayYuGiOh[index].archetype }}
+            </p>
+            <a href="#" class="btn btn-primary">
+                Go somewhere
+            </a>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                
+
             }
         },
 
@@ -15,7 +30,8 @@
         },
 
         props: {
-
+            ObjArrayYuGiOh: Array,
+            index: Number
         },
 
         created () {
@@ -27,4 +43,8 @@
 <style lang="scss" scoped>
     @use "../styles/variables.scss";
     @use "../styles/mixins.scss";
+
+    .card {
+        width: 18rem;
+    }
 </style>
